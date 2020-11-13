e844ffe32d04c18f35c66423fbfc2bd7d7e96ffb
aaronbbean <71397189+aaronbbean@users.noreply.github.com>
2020-11-13 09:47:34 -0600

    Update cryptogram.py

--- cryptogram.py
+++ cryptogram.py
@@ -2,20 +2,20 @@ from random import shuffle
 
 class Game:
   def __init__(self):
-    self.letters = list("zxcvbnmasdfghjklqwertyuiop")
+    self.letters = list("abcdefghijklmnopqrstuvwxyz")
     self.exclude = " .,'!()?"
 
   def Shuffle(self):
     shuffle(self.letters)
     player = input("What would you like in the Cryptogram?")
-    """Opens both files"""
+    #Opens both files
     files = open("Origanal.txt","w")
     files.write("you typed: ")
     files.write(player)
     files.close()
     fil = open("Original.txt","w")
     fil.write("the Crytogram is \n")
-    """the for loop get's the letter placement and put's it opisit place """
+    #the for loop get's the letter placement and put's it opisit place
     for letter in player:
       for number in range(26):
         if letter == self.letters[number]:
